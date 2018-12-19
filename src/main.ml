416ff41cdca2058b9036a618ae7fb5a73d45cef7
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-12-19 11:50:58 +0100

    Update main from the new API of Bechamel

--- src/main.ml
+++ src/main.ml
@@ -16,26 +16,14 @@ let dictionary value =
 
 let () =
   let ols = Analyze.ols ~bootstrap:0 ~r_square:true ~predictors:Measure.[|run|] in
-  let ransac = Analyze.ransac ~filter_outliers:false ~predictor:Measure.run in
   let instances = Instance.[minor_allocated; major_allocated; monotonic_clock] in
-  let ols, ransac =
-    Benchmark.all ~run:3000
-      ~quota:Benchmark.(s 1.)
-      instances
-      test
-    |> fun results ->
-    let ols = Analyze.all ols Instance.monotonic_clock results in
-    let ransac = Analyze.all ransac Instance.monotonic_clock results in
-    (ols, ransac) in
-  let ols_results = open_out "ols-results.json" in
-  let ols_json =
+  let results =
+    Benchmark.all ~run:3000 ~quota:Benchmark.(s 1.) instances test
+    |> Analyze.all ols Instance.monotonic_clock in
+  let output = open_out "results.json" in
+  let json =
     Json_encoding.construct (dictionary Analyze.OLS.Json.witness)
-      (Hashtbl.fold (fun name o acc -> (name, o) :: acc) ols []) in
-  let ransac_results = open_out "ransac-results.json" in
-  let ransac_json =
-    Json_encoding.construct (dictionary Analyze.RANSAC.Json.witness)
-      (Hashtbl.fold (fun name o acc -> (name, o) :: acc) ransac []) in
-  Ezjsonm.to_channel ols_results (Ezjsonm.wrap ols_json) ;
-  Ezjsonm.to_channel ransac_results (Ezjsonm.wrap ransac_json) ;
+      (Hashtbl.fold (fun name o acc -> (name, o) :: acc) results []) in
+  Ezjsonm.to_channel output (Ezjsonm.wrap json) ;
   ()
 
