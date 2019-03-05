701c2964eeaea42ce0ff6336de37f7e790110d65
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-03-05 09:55:49 +0100

    Simplify main.ml

--- src/main.ml
+++ src/main.ml
@@ -10,31 +10,6 @@ let make_list words =
 
 let test = Test.make_indexed ~name:"make a list of" ~fmt:"%s %d" ~args:[0; 10; 100; 400; 1000] make_list
 
-let dictionary value =
-  let open Json_encoding in
-  assoc value
-
-let dictionary_labelized t =
-  let open Json_encoding in
-  Json_encoding.custom
-    (fun l -> `O (List.map (fun (n, v) -> Label.to_string n, construct t v) l))
-    (fun v -> match v with
-       | `O l ->
-         let destruct n t v =
-           try Json_encoding.destruct t v
-           with Cannot_destruct (p, exn) -> raise (Cannot_destruct (`Field n :: p, exn)) in
-         List.map (fun (n, v) ->
-             let label = Json_encoding.destruct Label.Json.witness (`String n) in
-             label, destruct (label :> string) t v) l
-       | _ -> assert false)
-    ~schema:Json_schema.any
-
-let hashtbl_to_list hashtbl =
-  Hashtbl.fold (fun k v a -> (k, v) :: a) hashtbl []
-let hashtbl_map f hashtbl =
-  let ret = Hashtbl.create (Hashtbl.length hashtbl) in
-  Hashtbl.iter (fun k v -> Hashtbl.add ret k (f v)) hashtbl ; ret
-
 let benchmark () =
   let ols = Analyze.ols ~bootstrap:0 ~r_square:true ~predictors:Measure.[|run|] in
   let instances = Instance.[ minor_allocated
